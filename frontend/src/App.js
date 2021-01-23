0fab255299b57e133488dac78fed6d6a61633ffa
Nadern96 <nadernabilmansour@gmail.com>
2021-01-23 14:32:59 +0200

    Create Rating and Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import react from 'react';
 import data from './data';
+import Product from './components/Product';
 
 function App() {
   return (
@@ -19,27 +20,7 @@ function App() {
         {
           data.products.map(product => {
             return(
-              <div key={product._id} className="card">
-              <a href={`/products/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-              </a>
-
-              <div className="card-body">
-                <a href={`/products/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-
-                <div className="rating">
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star-half-o"></i></span>
-                  <span><i className="fa fa-star-o"></i></span>
-                </div>
-
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
+              <Product key={product._id} product={product}></Product>
           );
             
           })
