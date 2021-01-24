0681ee68f01b90b3f5efe66019880a9f39addbb1
Nadern96 <nadernabilmansour@gmail.com>
2021-01-24 13:34:30 +0200

    add redux to Product Screen

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -1,4 +1,5 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../actionTypes/productConstants';
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
+PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL} from '../actionTypes/productConstants';
 import Axios from 'axios';
 
 export const listProducts = () => async (dispatch) => {
@@ -15,6 +16,31 @@ export const listProducts = () => async (dispatch) => {
 		dispatch({
 			type: PRODUCT_LIST_FAIL,
 			payload: error.message
+		});
+	}
+}
+
+
+export const detailsProduct = (productId) => async (dispatch) => {
+	dispatch({
+		type: PRODUCT_DETAILS_REQUEST,
+		payload: productId
+	});
+	try {
+		const { data } = await Axios.get(`/api/products/${productId}`);
+		console.log('detailsProduct ',data);
+
+		dispatch({
+			type: PRODUCT_DETAILS_SUCCESS,
+			payload: data
+		});
+	} catch(error) {
+		dispatch({
+			type: PRODUCT_LIST_FAIL,
+			payload: error.response && error.response.data.message
+						? error.response.data.message
+						: error.message
+
 		});
 	}
 }
