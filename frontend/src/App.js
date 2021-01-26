2c6f27f42475c7e3d8366b30f4502eda405e9aaf
Nadern96 <nadernabilmansour@gmail.com>
2021-01-26 02:36:30 +0200

    implement add-to-cart action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import  CartScreen from './screens/CartScreen';
 
 
 function App() {
+  const cart = useSelector ((state) => state.cart);
+  const {cartItems} = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">Emazona</a>
+            <Link className="brand" to="/">Emazona</Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign in</a>
+            <Link to="/cart">Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>  
+              )}
+            </Link>
+            <Link to="/signin">Sign in</Link>
           </div>
         </header>
 
